cb8521e0ef0dc5ba6b4a0eb35932781f7289cc0f
Amanowsky <adamtroszczynski@outlook.com>
2023-11-30 16:56:57 +0100

    darkMode button,theTitle and background components added

---
+++ src/components/TheBackground.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-full w-full absolute top-0 -z-10" :class="color" >
+  <img class="w-full h-1/3" :src="bgSrc">
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgSrc: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String,
+      required: false
+    }
+  },
+};
+</script>
